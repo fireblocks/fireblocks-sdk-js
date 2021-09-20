96b2cad88041198fd08f01bf236f08705adec34b
Tomer Vilensky <tomerv@fireblocks.com>
2021-09-20 12:29:07 +0300

    Resend webhooks

--- src/fireblocks-sdk.ts
+++ src/fireblocks-sdk.ts
@@ -796,4 +796,11 @@ export class FireblocksSDK {
     public async freezeTransactionById(txId: string): Promise<OperationSuccessResponse> {
         return this.apiClient.issuePostRequest(`/v1/transactions/${txId}/freeze`, {});
     }
+
+    /**
+     * Resend failed webhooks
+     */
+    public async resendWebhooks(): Promise<ResendWebhooksResponse> {
+        return await this.apiClient.issuePostRequest("/v1/webhooks/resend", {});
+    }
 }
