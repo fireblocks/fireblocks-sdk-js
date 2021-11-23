f59863996ca49c7ad6483133954d58231937471c
Majid Lashgarian <majid.lashgarian@payconstruct.com>
2021-11-23 16:01:58 +0200

    delete the undefined header

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,71 +1,75 @@
-import { IAuthProvider } from "./iauth-provider";
-import { RequestOptions } from "./types";
-import axios, { AxiosInstance } from "axios";
+import { IAuthProvider } from "./iauth-provider"
+import { RequestOptions } from "./types"
+import axios, { AxiosInstance } from "axios"
 
 export class ApiClient {
-    private axiosInstance: AxiosInstance;
+  private axiosInstance: AxiosInstance
 
-    constructor(private authProvider: IAuthProvider, private apiBaseUrl: string) {
-        this.axiosInstance = axios.create({
-            baseURL: this.apiBaseUrl
-        });
+  constructor(private authProvider: IAuthProvider, private apiBaseUrl: string) {
+    this.axiosInstance = axios.create({
+      baseURL: this.apiBaseUrl,
+    })
+  }
+
+  public async issueGetRequest(path: string, pageMode: boolean = false) {
+    const token = this.authProvider.signJwt(path)
+    const res = await this.axiosInstance.get(path, {
+      headers: {
+        "X-API-Key": this.authProvider.getApiKey(),
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    if (pageMode) {
+      return {
+        transactions: res.data,
+        pageDetails: {
+          prevPage: res.headers["prev-page"] ? res.headers["prev-page"].toString() : "",
+          nextPage: res.headers["next-page"] ? res.headers["next-page"].toString() : "",
+        },
+      }
     }
 
-    public async issueGetRequest(path: string, pageMode: boolean = false) {
-        const token = this.authProvider.signJwt(path);
-        const res = await this.axiosInstance.get(path, {
-            headers: {
-                "X-API-Key": this.authProvider.getApiKey(),
-                "Authorization": `Bearer ${token}`
-            }
-        });
+    return res.data
+  }
 
-        if (pageMode) {
-            return {
-                transactions: res.data,
-                pageDetails: {
-                    prevPage: res.headers["prev-page"] ? res.headers["prev-page"].toString() : "",
-                    nextPage: res.headers["next-page"] ? res.headers["next-page"].toString() : "",
-                }
-            };
-        }
+  public async issuePostRequest(path: string, body: any, requestOptions?: RequestOptions) {
+    const token = this.authProvider.signJwt(path, body)
 
-        return res.data;
-    }
+    const idempotencyKey = requestOptions?.idempotencyKey
 
-    public async issuePostRequest(path: string, body: any, requestOptions?: RequestOptions) {
-        const token = this.authProvider.signJwt(path, body);
+    return (
+      await this.axiosInstance.post(path, body, {
+        headers: {
+          "X-API-Key": this.authProvider.getApiKey()
+        },
+      })
+    ).data
+  }
 
-        const idempotencyKey = requestOptions?.idempotencyKey;
+  public async issuePutRequest(path: string, body: any) {
+    const token = this.authProvider.signJwt(path, body)
 
-        return (await this.axiosInstance.post(path, body, {
-            headers: {
-                "X-API-Key": this.authProvider.getApiKey(),
-                "Authorization": `Bearer ${token}`,
-                "Idempotency-Key": idempotencyKey
-            }
-        })).data;
-    }
+    return (
+      await this.axiosInstance.put(path, body, {
+        headers: {
+          "X-API-Key": this.authProvider.getApiKey(),
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).data
+  }
 
-    public async issuePutRequest(path: string, body: any) {
-        const token = this.authProvider.signJwt(path, body);
+  public async issueDeleteRequest(path: string) {
+    const token = this.authProvider.signJwt(path)
 
-        return (await this.axiosInstance.put(path, body, {
-            headers: {
-                "X-API-Key": this.authProvider.getApiKey(),
-                "Authorization": `Bearer ${token}`
-            }
-        })).data;
-    }
-
-    public async issueDeleteRequest(path: string) {
-        const token = this.authProvider.signJwt(path);
-
-        return (await this.axiosInstance.delete(path, {
-            headers: {
-                "X-API-Key": this.authProvider.getApiKey(),
-                "Authorization": `Bearer ${token}`
-            }
-        })).data;
-    }
+    return (
+      await this.axiosInstance.delete(path, {
+        headers: {
+          "X-API-Key": this.authProvider.getApiKey(),
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).data
+  }
 }
