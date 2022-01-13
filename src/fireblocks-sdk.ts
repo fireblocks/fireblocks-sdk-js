8eb683c8df9a375498cb15696d0618ffb6891447
yinonhever <yinon@fireblocks.com>
2022-01-13 12:20:07 +0200

    refactor to post

--- src/fireblocks-sdk.ts
+++ src/fireblocks-sdk.ts
@@ -193,7 +193,7 @@ export class FireblocksSDK {
      */
     public async setNetworkConnectionRoutingPolicy(connectionId: string, routingPolicy?: RoutingPolicy) {
         const body = { routingPolicy };
-        return await this.apiClient.issuePatchRequest(`/v1/network_connections/${connectionId}/set_routing_policy`, body);
+        return await this.apiClient.issuePostRequest(`/v1/network_connections/${connectionId}/set_routing_policy`, body);
     }
 
     /**
@@ -220,7 +220,7 @@ export class FireblocksSDK {
      */
     public async setNetworkIdRoutingPolicy(networkId: string, routingPolicy?: RoutingPolicy) {
         const body = { routingPolicy };
-        return await this.apiClient.issuePatchRequest(`/v1/network_ids/${networkId}/set_routing_policy`, body);
+        return await this.apiClient.issuePostRequest(`/v1/network_ids/${networkId}/set_routing_policy`, body);
     }
 
     /**
