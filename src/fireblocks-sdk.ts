00a85e6ded3d1ba479e45df528b26726b5187086
Shir Gerasi <shir@fireblocks.com>
2023-03-29 16:37:10 +0300

    Add getCollateralAccountByMainExchangeAccountId functionality

--- src/fireblocks-sdk.ts
+++ src/fireblocks-sdk.ts
@@ -69,8 +69,8 @@ import {
     GetNFTsFilter,
     PublicKeyInformation,
     DropTransactionResponse,
-    TokenLink, 
-    TokenLinkPermissionEntry, 
+    TokenLink,
+    TokenLinkPermissionEntry,
     IssueTokenRequest,
 } from "./types";
 import { AxiosProxyConfig, AxiosResponse } from "axios";
@@ -1118,6 +1118,15 @@ export class FireblocksSDK {
     public async settlement(settlementRequest: SettlementRequest, requestOptions?: RequestOptions): Promise<SettlementResponse> {
         return await this.apiClient.issuePostRequest(`/v1/off_exchange/settlements/trader`, settlementRequest, requestOptions);
     }
+
+    /**
+     * Gets a single collateral account by ID
+     * @param mainExchangeAccountId The exchange account ID that connect to the collateral account
+     */
+     public async getCollateralAccountByMainExchangeAccountId(mainExchangeAccountId: string): Promise<ExchangeResponse> {
+        return await this.apiClient.issueGetRequest(`/v1/off_exchange/collateral_accounts/${mainExchangeAccountId}`);
+    }
+
     /**
      * Set Fee Payer configuration
      * @param feePayerConfiguration
