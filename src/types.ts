6a01b3397d16701a66883dd31e10ed5f267262a5
barakfireblocks <130898250+barakfireblocks@users.noreply.github.com>
2023-04-16 16:41:41 +0300

    added getMaxBip44IndexUsed (#161)

--- src/types.ts
+++ src/types.ts
@@ -745,6 +745,11 @@ export interface MaxSpendableAmountResponse {
     maxSpendableAmount: string;
 }
 
+export interface MaxBip44IndexUsedResponse {
+    maxBip44AddressIndexUsed?: number;
+    maxBip44ChangeAddressIndexUsed?: number;
+}
+
 export interface VaultAccountsFilter {
     namePrefix?: string;
     nameSuffix?: string;
