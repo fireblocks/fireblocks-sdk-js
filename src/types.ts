3fa9921ccbadf730c9a8dc4a5301c3263a1c0c92
Chidozie Ogbo <dosky.ogbo91@gmail.com>
2023-11-09 08:00:33 +0100

    Update transaction response metadata using API reference

--- src/types.ts
+++ src/types.ts
@@ -645,6 +645,7 @@ export interface FeePayerInfo {
 export interface TransactionResponse {
     id: string;
     assetId: string;
+    operation: string;
     source: TransferPeerPathResponse;
     destination: TransferPeerPathResponse;
     amount: number;
@@ -705,6 +706,9 @@ export interface FeeInfo {
 export interface TransactionResponseDestination {
     amount?: string;
     amountUSD?: string;
+    customerRefId?: string;
+    destinationAddress?: string;
+    destinationAddressDescription?: string;
     amlScreeningResult?: AmlScreeningResult;
     destination?: TransferPeerPathResponse;
     authorizationInfo?: AuthorizationInfo;
