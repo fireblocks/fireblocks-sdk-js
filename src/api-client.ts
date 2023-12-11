92411ac9c68d3a2d3fc760b5f1f1cf9051706fa6
Alon Bar <abar@fireblocks.com>
2023-12-11 22:58:44 +0200

    fixing COR-44075 - After the latest axios upgrade from 0.27.2 to 1.6.0 n urls with ? but not params the ? character is omitted and we get are reaching a siguration that we are signing https://path/to/somwehere? but sending the request with https://path/to/somewhere

--- src/api-client.ts
+++ src/api-client.ts
@@ -36,7 +36,8 @@ export class ApiClient {
         return userAgent;
     }
 
-    public async issueGetRequestForTransactionPages(path: string): Promise<TransactionPageResponse> {
+    public async issueGetRequestForTransactionPages(rawPath: string): Promise<TransactionPageResponse> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path);
         const res = await this.axiosInstance.get(path, {
             headers: {"Authorization": `Bearer ${token}`}
@@ -50,7 +51,8 @@ export class ApiClient {
         };
     }
 
-    public async issueGetRequest<T>(path: string): Promise<T> {
+    public async issueGetRequest<T>(rawPath: string): Promise<T> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path);
         const res = await this.axiosInstance.get(path, {
             headers: {"Authorization": `Bearer ${token}`}
@@ -58,7 +60,8 @@ export class ApiClient {
         return res.data;
     }
 
-    public async issuePostRequest<T>(path: string, body: any, requestOptions?: RequestOptions): Promise<T> {
+    public async issuePostRequest<T>(rawPath: string, body: any, requestOptions?: RequestOptions): Promise<T> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path, body);
         const headers: any = {"Authorization": `Bearer ${token}`};
         const idempotencyKey = requestOptions?.idempotencyKey;
@@ -75,7 +78,8 @@ export class ApiClient {
         return response.data;
     }
 
-    public async issuePutRequest<T>(path: string, body: any): Promise<T> {
+    public async issuePutRequest<T>(rawPath: string, body: any): Promise<T> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path, body);
         const res = (await this.axiosInstance.put<T>(path, body, {
             headers: {"Authorization": `Bearer ${token}`}
@@ -83,7 +87,8 @@ export class ApiClient {
         return res.data;
     }
 
-    public async issuePatchRequest<T>(path: string, body: any): Promise<T> {
+    public async issuePatchRequest<T>(rawPath: string, body: any): Promise<T> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path, body);
         const res = (await this.axiosInstance.patch<T>(path, body, {
             headers: {"Authorization": `Bearer ${token}`}
@@ -91,11 +96,18 @@ export class ApiClient {
         return res.data;
     }
 
-    public async issueDeleteRequest<T>(path: string): Promise<T> {
+    public async issueDeleteRequest<T>(rawPath: string): Promise<T> {
+        const path = normalizePath(rawPath)
         const token = this.authProvider.signJwt(path);
         const res = (await this.axiosInstance.delete<T>(path, {
             headers: {"Authorization": `Bearer ${token}`}
         }));
         return res.data;
     }
+}
+
+// This function allows backward compatibility with previous version of axois that did not omit ? for 
+// urls with no params. This function will make sure we are omitting the ? before signing it
+function normalizePath(path: string) {
+    return path.replace(/\?$/, "");
 }
