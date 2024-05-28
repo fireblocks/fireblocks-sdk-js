35befcb9d1b1b03271b78656372580dcad96314c
phanigovindaraju <166809008+phanigovindaraju@users.noreply.github.com>
2024-05-28 17:33:03 +1000

    Update Tokenization DTOs and deprecate unused route (#276)
    * Remove constructor endpoint
    
    * Remove constructorParams
    
    * Change EVMTokenCreateParamsDto
    
    * Update parameter name
    
    * Adding back constructor params to support integration tests
    
    * Removing constructor params

--- src/types.ts
+++ src/types.ts
@@ -1799,8 +1799,7 @@ interface VendorDto {
 export interface ContractDeployRequest {
     assetId: string;
     vaultAccountId: string;
-    constructorParameters?: object[];
-    deployFunctionParameters?: object[];
+    deployFunctionParams?: object[];
 }
 
 export interface SupportedBlockchain {
@@ -1988,7 +1987,7 @@ export type ParameterWithValueList = ParameterWithValue[] | ParameterWithValueLi
 
 interface EVMTokenCreateParamsDto {
     contractId: string;
-    constructorParams?: Array<ParameterWithValue>;
+    deployFunctionParams?: Array<ParameterWithValue>;
 }
 
 export interface ReadCallFunctionDto {
