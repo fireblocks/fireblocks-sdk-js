7db01dd62f04f6669e432e74dd5ff3e389aae5f6
Yuval Niezni <yniezni@fireblocks.com>
2024-11-19 16:49:57 +0200

    fix url

--- src/ncw-api-client.ts
+++ src/ncw-api-client.ts
@@ -171,6 +171,6 @@ export class NcwApiClient implements NcwSdk {
     }
 
     public async deleteSigningAlgorithm(walletId: string, algorithm: SigningAlgorithm): Promise<void> {
-        return await this.apiClient.issueGetRequest(`/v1/ncw/${walletId}/remove_signing_algorithm/${algorithm}`);
+        return await this.apiClient.issueGetRequest(`${this.NCW_BASE_PATH}/${walletId}/remove_signing_algorithm/${algorithm}`);
     }
 }
